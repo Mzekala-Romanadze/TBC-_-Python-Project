ba63fdc3bb5632c9e1ddcee7ae5039e752ba6b1a
Mzekala-Romanadze <mzekala.romanadze8@gmail.com>
2024-03-04 23:12:44 +0400

    First commit

---
+++ Assignment_1/Task_1.py
@@ -0,0 +1,7 @@
+# This program tells about user's full name and age.
+
+name = str(input("What is your first name? "))
+surname = str(input("What is your last name? "))
+age = int(input("How old are you? "))
+print("Hello, ", name, surname)
+print("You're ", age, "years old.")
---
+++ Assignment_1/Task_2.py
@@ -0,0 +1,7 @@
+# This program counts the age of the user.
+
+name = str(input("What is your name? "))
+year = int(input("In which year were you born? "))
+age = 2024 - year
+print("Hello,", name)
+print("You're ", age, "years old.")
