d4c7ddcb597cd9086ea12f9a43e3dc946a8d68d1
Mzekala-Romanadze <mzekala.romanadze8@gmail.com>
2024-03-05 00:31:52 +0400

    Second commit

--- Assignment_1/Task_1.py
+++ Assignment_1/Task_1.py
@@ -3,5 +3,9 @@
 name = str(input("What is your first name? "))
 surname = str(input("What is your last name? "))
 age = int(input("How old are you? "))
-print("Hello, ", name, surname)
-print("You're ", age, "years old.")
+
+if age <= 0:
+    print("Enter positive number for your age")
+else:
+    print("Hello, ", name, surname)
+    print("You're ", age, "years old.")
