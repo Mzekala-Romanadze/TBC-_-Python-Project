f32cb3b39c0bdfb981c009b72556e20eae20d67a
Mzekala-Romanadze <mzekala.romanadze8@gmail.com>
2024-03-04 23:18:20 +0400

    First commit

---
+++ Assignment_3/Task_1.py
@@ -0,0 +1,8 @@
+# This program gets two numbers x and y from the user
+# and prints the result of x raised to the power of y.
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+result = x ** y
+
+print("The result of x raised to the power of y is ", result)
---
+++ Assignment_3/Task_4.py
@@ -0,0 +1,6 @@
+# This program gets the year, month and day from the user (it is the date when
+# the user bought a bitcoin).
+# The program gets also an amount of money the user paid for the bitcoin on that date (in dollars).
+# The program prints the amount of money in dollars which the user won or lost
+# taking into consideration the price of bitcoin now.
+# The program using module forex-python for Foreign exchange rates and currency conversion.
