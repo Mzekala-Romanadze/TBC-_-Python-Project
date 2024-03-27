363b50f5ec4edec2daaa49549d4063c0b9c29ddd
Mzekala-Romanadze <mzekala.romanadze8@gmail.com>
2024-03-27 13:55:20 +0400

    Update Assignment_8/Task_1.py
    Co-authored-by: Mikheil Lomidze <lomidzemikheili@gmail.com>

--- Assignment_8/Task_1.py
+++ Assignment_8/Task_1.py
@@ -6,7 +6,7 @@ user_input = str(input("Enter text: "))
 
 even_index = ""
 for c in range(len(user_input)):
-    if c % 2 == 0 and "e" not in user_input[c]:
+    if c % 2 == 0 and "e" != user_input[c]:
         even_index += user_input[c]
 
 print(even_index)
