95ef6d212b202b5c1cbce546c2fca1874f37530b
Mzekala Romanadze <mzekala.romanadze8@gmail.com>
2024-04-01 17:52:12 +0400

    Ready to merge

--- Assignment_1/Task_1.py
+++ Assignment_1/Task_1.py
@@ -1,4 +1,5 @@
-# This program tells about user's full name and age.
+# This program gets answers from the user
+# and prints user's full name and age.
 
 name = str(input("What is your first name? "))
 surname = str(input("What is your last name? "))
