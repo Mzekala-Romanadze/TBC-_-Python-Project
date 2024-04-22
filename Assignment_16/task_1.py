8c3fdbda65a40a362810d61c2b7a726cdc89b321
Mzekala-Romanadze <mzekala.romanadze8@gmail.com>
2024-04-22 18:54:15 +0400

    Task_1: first commit after feedback

--- Assignment_16/task_1.py
+++ Assignment_16/task_1.py
@@ -15,32 +15,25 @@
 # temperatures of the week (based on the arithmetic mean of each day).
 
 
-def calculate_daily_means(days_of_week, temperatures):
+def get_daily_and_week_temperature_stats(days_of_week, temperatures):
+    daily_means_sum = 0
     for c in range(len(temperatures)):
-        for _ in range(1):
-            print(f"Day: {days_of_week[c]}")
-            print(f"Temperatures: {temperatures[c]}")
-            print(f"The arithmetic mean of the temperatures: {sum(temperatures[c]) / len(temperatures[c])}")
-            print(f"The minimum temperature: {min(temperatures[c])}")
-            print(f"The maximum temperature: {max(temperatures[c])}")
+        print(f"Day: {days_of_week[c]}")
+        print(f"Temperatures: {temperatures[c]}")
+        print(f"The arithmetic mean of the temperatures: {sum(temperatures[c]) / len(temperatures[c])}")
+        print(f"The minimum temperature: {min(temperatures[c])}")
+        print(f"The maximum temperature: {max(temperatures[c])}")
+        daily_means_sum += sum(temperatures[c]) / len(temperatures[c])
 
-
-def calculate_week_mean(temperatures):
-    sum_mean_temps = 0
-    for c in range(len(temperatures)):
-        for i in range(1):
-            sum_mean_temps += sum(temperatures[c]) / len(temperatures[c])
-    week_mean = sum_mean_temps / len(temperatures)
-    return week_mean
+    week_mean = daily_means_sum / len(temperatures)
+    print(f"\nThe arithmetic mean of temperatures of the week: {week_mean}")
 
 
 def main():
     temperatures = ((33, 34, 28), (24, 31, 27), (24, 23, 27), (28, 32, 34), (33, 21, 28), (20, 25, 31), (21, 31, 28))
     days_of_week = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    week_mean = calculate_week_mean(temperatures)
-    calculate_daily_means(days_of_week, temperatures)
 
-    print(f"\nThe arithmetic mean of temperatures of the week: {week_mean}")
+    get_daily_and_week_temperature_stats(days_of_week, temperatures)
 
 
 if __name__ == "__main__":
