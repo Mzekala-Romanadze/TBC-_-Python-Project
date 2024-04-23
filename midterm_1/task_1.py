93413f77f957e40d7e5232d1137d996210a0ca68
Mzekala-Romanadze <mzekala.romanadze8@gmail.com>
2024-04-23 12:31:41 +0400

    Midterm: first commit after feedback

--- midterm_1/task_1.py
+++ midterm_1/task_1.py
@@ -1,5 +1,21 @@
-name = input("Enter your first name and last name: ")
-age = int(input("Enter your age: "))
+# Task_1: (5 ქულა) Დაწერეთ პროგრამა რომელიც მომხმარებელს მოსთხოვს
+# შეიყვანოს შემდეგი მონაცემები: სახელი, გვარი და ასაკი.
+# Ეს ინფორმაცია Პროგრამამ ეკრენზე უნდა დაბეჭდოს შემდეგ ფორმატში:
+# Hello სახელი გვარი.
+# Age: ასაკი.
 
-print("Hello", name)
-print("Age: ", age)
+
+def main():
+    name = input("Enter name: ")
+    surname = input("Enter surname: ")
+    age = input("Enter age: ")
+
+    if not age.isnumeric():
+        print("Please, enter correct age.")
+        exit(1)
+    print(f"Hello {name} {surname}.")
+    print(f"Age: {age}.")
+
+
+if __name__ == "__main__":
+    main()
--- midterm_1/task_2.py
+++ midterm_1/task_2.py
@@ -1,11 +1,31 @@
-n = int(input("Enter n in range 100-1000: "))
+# Task_2: (8 ქულა) Დაწერეთ პროგრამა რომელიც მოხმარებელს მოსთხოვს
+# შეიყვანოს მთელი რიცხვი n, სადაც 100<= n < 1000.
+# Თუ მომხმარებელი შეიყვანს არასწორ რიცხვს ეკრანზე დაბეჭდეთ
+# რომ პროგრამას გადმოეცა არასწორი რიცხვი და შეწყვიტეთ პროგრამის მუშაობა.
+# Თუ მომხმარებელი შეიყვანს სწორ რიცხვს, პროგრამამ უნდა იპოვოს ყველა
+# 13 ის ჯერადი რიცხვი 1-დან n-მდე. Ეკრანზე დაბეჭდოს
+# ეს რიცხვები და მათი საერთო რაოდენობა.
 
-if 100 <= n <= 1000:
-    counter = []
-    for i in range(1, n + 1):
-        if i % 13 == 0:
-            counter.append(i)
-    print(counter)
-    print(len(counter))
-else:
-    print("Wrong number. Please, enter n in range 100-1000.")
+BASE_NUMBER = 13
+
+
+def find_multiples(n):
+    multiples = 0
+    for multiple in range(1, n):
+        if multiple % BASE_NUMBER == 0:
+            print(multiple)
+            multiples += 1
+    return multiples
+
+
+def main():
+    n = int(input("Enter n [100-1000): "))
+    if not 100 <= n < 1000:
+        print("Please, enter n in range [100, 1000).")
+        return None
+    number_of_multiples = find_multiples(n)
+    print("Total number of multiples: ", number_of_multiples)
+
+
+if __name__ == "__main__":
+    main()
