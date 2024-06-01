38038cb4889ff182543d14a50ba98b1131227e73
Mzekala-Romanadze <mzekala.romanadze8@gmail.com>
2024-06-01 19:45:46 +0400

    Assignment 6: small technical changes in all files

--- Assignment_6/Task_1.py
+++ Assignment_6/Task_1.py
@@ -3,14 +3,13 @@
 # and does not repeat the same multiplication from previous columns.
 # The problem is solved by using While loop.
 
-i = 1
-while i < 10:
-    j = 1
-    while i < 10:
-        result = i * j
-        if result > i ** 2:
-            break
-        print(f"{j} * {i} = {result}", end="  ")
-        j += 1
-    print(" ")
-    i += 1
+x = 1
+while x < 10:
+    y = 1
+    while y <= x:
+        result = x * y
+        space = "  "
+        print(f"{y} * {x} = {result}{space}", end=" ")
+        y += 1
+    print("  ")
+    x += 1
